2b708b9221457c21b9eae0543e83c4c881c2ba27
Xander Lubbe BBD <xanderl@bbd.co.za>
2023-06-27 14:58:54 +0200

    Linting

--- resource-server/public/js/welcome.js
+++ resource-server/public/js/welcome.js
@@ -17,51 +17,45 @@ function changeImage() {
     firstImage.style.display = "none";
     secondImage.style.display = "block";
     endLoop += 1;
-    setTimeout(changeImage, 5000); 
+    setTimeout(changeImage, 5000);
   } else {
     firstImage.style.display = "block";
     secondImage.style.display = "none";
     endLoop += 1;
-    setTimeout(changeImage, 5000); 
+    setTimeout(changeImage, 5000);
   }
 }
 
 changeImage();
-  
-  
 
+$(document).bind("contextmenu", function (e) {
+  e.preventDefault();
+});
 
+$(document).keydown(function (event) {
+  switch (event.keyCode) {
+    case 123:
+      return false; // Prevent from F12
+    case 73:
+      if (event.ctrlKey && event.shiftKey) {
+        return false; // Prevent from ctrl+shift+i
+      }
+      break;
+    case 85:
+      if (event.ctrlKey) {
+        return false; // Prevent from ctrl+u
+      }
+      break;
+    case 67:
+      if (event.ctrlKey) {
+        return false; // Prevent from ctrl+c
+      }
+      break;
+    case 88:
+      if (event.ctrlKey) {
+        return false; // Prevent from ctrl+x
+      }
+      break;
+  }
 
-
-
-// $(document).bind("contextmenu", function (e) {
-//     e.preventDefault();
-//   });
-  
-//   $(document).keydown(function (event) {
-//     switch (event.keyCode) {
-//         case 123:
-//           return false; // Prevent from F12
-//         case 73:
-//           if (event.ctrlKey && event.shiftKey) {
-//             return false; // Prevent from ctrl+shift+i
-//           }
-//           break;
-//         case 85:
-//           if (event.ctrlKey) {
-//             return false; // Prevent from ctrl+u
-//           }
-//           break;
-//         case 67:
-//           if (event.ctrlKey) {
-//             return false; // Prevent from ctrl+c
-//           }
-//           break;
-//         case 88:
-//           if (event.ctrlKey) {
-//             return false; // Prevent from ctrl+x
-//           }
-//           break;
-//       }
-      
-//   });
+});
