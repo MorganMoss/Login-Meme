5eac5ee522a01e2a7f1afe5cd1b870a303b475b5
Karl Haupt <karl@bbd.co.za>
2023-06-29 15:22:11 +0200

    Minor fix to not crash website

--- resource-server/middlewares/auth.js
+++ resource-server/middlewares/auth.js
@@ -10,9 +10,16 @@ exports.isAuthenticatedUser = async (req, res, next) => {
         return next(new ErrorHandler('Login with email and password first to access this resources.', 401));
     }
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const result = await findUserByID(decoded.id);
-    req.user = result.user;
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const result = await findUserByID(decoded.id);
+        req.user = result.user;
+    } catch(err) {
+        res.status(500).json({
+            success: false,
+            message: 'Invalid Web token'
+        });
+    }
 
     next();
 };
